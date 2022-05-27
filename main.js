b523809dfa54e4ca68cb08b24b3eac842c8c0c02
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2022-05-26 22:21:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 const ElementContainer = document.querySelector(".container");
-let data;
+const ElementCount = document.querySelector(".info .count");
+const ElementOptions = document.querySelector(".backout");
+const ElementLocation = document.querySelector(".displayLocation .text");
+const ElementChoiceLocation = document.querySelector(".containerLocation ul");
 
-let filter = []
-fetch('./Recursos/stays.json').then(
+let textChoice;
+let newData;
+let consulta={
+    city:[],
+    country:[]
+};
+
+let filterChoice = []
+const promisse = fetch('./Recursos/stays.json').then(
     response => response.json()
 ).then(
     data => {
-        let newData = data.filter((item)=>{
-            let contain=false;
-            let op=["city","country","beds"]
-            for(let j in op){
-                if(filter.includes(item[op[j]]))
-                    contain=true ;
-            }
-            if(filter.length>0)
-                return contain;
-            else
-                return true;
-
-        });
-        for (let pos in newData) {
-            addEntidade(newData[pos])
-        }
-
+        filtrar(data);
     }
 )
+Promise.resolve(promisse).then(()=>{
+    textChoice = [...new Set(newData.map(item=>{return item["city"]} ))]
+    console.log(textChoice)
+    for(i in textChoice){
+        ElementChoiceLocation.innerHTML +=`
+            <input onchange="choiceLocation(this)"  type="checkbox"  value="${textChoice[i]}" id="option${i}">
+                <label for="option${i}">${textChoice[i]}</label>
+            </input>
+        `;
+    }
+     
 
-
+})
 
 function addEntidade(value) {
     ElementContainer.innerHTML += `
@@ -45,10 +50,51 @@ function addEntidade(value) {
 }
 
 function openOption(e){
-    const ElementOptions = document.querySelector(".backout");
     ElementOptions.style.display = "block"; 
     ElementOptions.addEventListener("click",(e)=>{
         if(!document.querySelector(".backout .display").contains(e.target))
             ElementOptions.style.display = "none"; 
     })
+}
+
+function choiceLocation(e){
+    let arrayChecked = Array.from(e.parentNode.children).map((elem,pos)=>{
+        if(elem.checked) return  pos/2;
+    }).filter((elem)=> {return elem!= undefined})
+    console.log(arrayChecked)
+    filterChoice = []
+    if(arrayChecked.length ==0){
+        filterChoice = []
+        ElementLocation.innerHTML = "Add Location"
+    }
+    else for(let i =0;i<arrayChecked.length;i++){
+        i==0?ElementLocation.innerHTML = textChoice[arrayChecked[i]]: ElementLocation.innerHTML += ", "+textChoice[arrayChecked[i]];
+        filterChoice.push(textChoice[arrayChecked[i]]);
+    } 
+    filtrar(newData);
+}
+
+function filtrar(data){
+    console.log(data)
+    console.log(filterChoice)
+    newData = data.filter((item)=>{
+        let contain=false;
+        let op=["city","country","beds"]
+        for(let j in op){
+            if(filterChoice.includes(item[op[j]]))
+                contain=true ;
+        }
+        if(filterChoice.length>0)
+            return contain;
+        else
+            return true;
+
+    });
+    console.log(newData)
+    ElementCount.innerHTML = newData.length >1? newData.length+" stays":newData.length+" stay";
+    ElementContainer.innerHTML=""
+    for (let pos in newData) {
+        addEntidade(newData[pos])
+    }
+    newData=data;
 }
